b7442d2feceec3504fadc745894f8298c3ff34bf
mlab1 <mlabrecque@gmail.com>
2022-09-20 07:18:06 -0400

    Playing with lists

---
+++ src/main/kotlin/misc/Lists1.kt
@@ -0,0 +1,11 @@
+package misc
+
+fun main()
+{
+    val name : String = "Robert"
+    val age = "99"
+    val nickName = "Stinky"
+    val list1 = listOf(name) + age + nickName
+    val iist2 = list1 + "ready" + "to" + "Eat"
+
+}
